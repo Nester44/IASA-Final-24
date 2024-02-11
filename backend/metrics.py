ef09e79bfd75b07dca088c0d233013cd4c9d7957
MytsV <mytsvictoria@gmail.com>
2024-02-11 10:31:16 +0200

    More keywords

--- backend/metrics.py
+++ backend/metrics.py
@@ -14,7 +14,7 @@ def get_keywords(post):
   deduplication_threshold = 0.5
   deduplication_algo = 'seqm'
   windowSize = 3
-  numOfKeywords = 20
+  numOfKeywords = 100
 
   kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, dedupFunc=deduplication_algo, windowsSize=windowSize, top=numOfKeywords, features=None, stopwords=stopwords_specific)
   return kw_extractor.extract_keywords(post)
